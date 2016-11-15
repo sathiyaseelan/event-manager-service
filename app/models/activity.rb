e10c52f2dea51192a764a0101cf5ca430f0a8540
sathiyaseelan <ssstarlet@gmail.com>
2016-11-15 23:46:04 +0530

    changed from decimal to string for price in activity.rb

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,6 @@ class Activity
   include Mongoid::Document
   field :title, type: String
   field :schedule_date, type: DateTime
-  field :price, type: Decimal
+  field :price, type: String
   field :description, type: String
 end
