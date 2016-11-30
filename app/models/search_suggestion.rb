cf0165f8dc57a5e9a403e1bf70d2d93653f58971
sathiyaseelan <ssstarlet@gmail.com>
2016-11-30 10:41:08 +0530

    Handled invalid email scnearios in autocomplete

--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -6,7 +6,7 @@ class SearchSuggestion
 
   def self.seed_emails_to_redis
     User.all.each do |user|
-      email = user.email.downcase
+      email = user.email
       add_email_to_redis(email)
     end
   end
@@ -17,15 +17,19 @@ class SearchSuggestion
 
   def self.clear_emails
     keys = $redis.keys "search-suggestions:*"
-    count = $redis.del keys
-    puts "Total number of keys cleared:#{count}"
+    unless keys.empty?
+      count = $redis.del keys
+      puts "Total number of keys cleared:#{count}"
+    end
   end
 
   private
   def self.add_email_to_redis(email)
-    3.upto(email.length-1) do |c|
-      prefix = email[0, c]
-      $redis.zadd "search-suggestions:#{prefix}", 1, email
+    if email && !email.empty?
+      3.upto(email.length-1) do |c|
+        prefix = email[0, c]
+        $redis.zadd "search-suggestions:#{prefix}", 1, email
+      end
     end
   end
 end
